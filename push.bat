aa41e32958aa422f003f4f5a67cb3656f1f62bee
Sam <samroux808@gmail.com>
2022-02-12 17:14:19 -0600

    Commit on Sat 02/12/2022 at 17:14:19.31:  pepe

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Beginning push...
-set /p commitMessage=Enter a commit message:
+set /p commitMessage="Enter a commit message: "
 git add .
 git commit -m "Commit on %date% at %time%: %commitMessage%"
 git push github main

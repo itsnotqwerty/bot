f1bc6f6b4b90c3043e3d87b78d4ecfd3b5b59fb3
Sam <samroux808@gmail.com>
2022-02-12 17:13:13 -0600

    Commit on Sat 02/12/2022 at 17:13:13.54: Enter a commit message:

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo on
+echo Beginning push...
+set commitMessage=Enter a commit message:
+git add .
+git commit -m "Commit on %date% at %time%: %commitMessage%"
+git push github main
+echo Push finished.
